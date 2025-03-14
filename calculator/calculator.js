9d138cebdfddb6f989e5d2a6b15aef0383d2bf12
jjetblue16-was-here <jaydenzcho@gmail.com>
2025-03-13 20:16:46 -0400

    added to try js big decimal multi

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,4 +1,4 @@
-let currentNumber=0;
+let currentNumber="0";
 
 let numberStorage;
 
@@ -6,25 +6,26 @@ let whatOperatorClicked;
 
 let decimalClick=false;
 
-let divideForDecimal=10;
-
 let lastNumber;
 
 function decimal()  {
     if(decimalClick==false) {
-        document.getElementById("calNumber").textContent=currentNumber+".";
+        currentNumber=currentNumber+".";
+        document.getElementById("calNumber").textContent=currentNumber;
     }
     decimalClick=true;
 }
 
 function percent()  {
-    currentNumber=currentNumber/100;
+    /*currentNumber=currentNumber/100;
     document.getElementById("calNumber").textContent=currentNumber;
+    use divide_bd*/
 }
 
 function negate()   {
-    currentNumber=currentNumber*-1;
+    /*currentNumber=currentNumber*-1;
     document.getElementById("calNumber").textContent=currentNumber;
+    use multiply_bd*/
 }
 
 function operatorClicked(operator)  {
@@ -64,21 +65,19 @@ function equal() {
         currentNumber=lastNumber;
         lastNumber=undefined;
     }
-    let stringNumberStorage=numberStorage.toString();
-    let stringCurrent=currentNumber.toString();
     if (whatOperatorClicked=="plus") {
-        result=bd_add(stringNumberStorage, stringCurrent);
+        result=decimal_add(numberStorage, currentNumber);
     }
     else if (whatOperatorClicked=="minus") {
         result=numberStorage-currentNumber;
     }
     else if (whatOperatorClicked=="multi") {
-        result=numberStorage*currentNumber;
+        result=decimal_multi(numberStorage, currentNumber);
     }
     else if (whatOperatorClicked=="divide") {
         result=numberStorage/currentNumber;
     }
-    numberStorage=Number(result);
+    numberStorage=result;
     lastNumber=currentNumber;
     document.getElementById("calNumber").textContent=result;
     currentNumber = 0;
@@ -87,34 +86,25 @@ function equal() {
 
 function numberButton(number)   {
     lastNumber=undefined;    
-    if(decimalClick)    {
-        currentNumber=currentNumber+number/divideForDecimal;
-        divideForDecimal=divideForDecimal*10;
-        currentNumber=currentNumber.toPrecision(10)*1;
+    if(currentNumber=="0")    {
+        currentNumber=number;
     }
     else    {
-        if(currentNumber<0) {
-            currentNumber=currentNumber*10-number;
-        }
-        else    {
-            currentNumber=currentNumber*10+number;
-        }
+        currentNumber=currentNumber+number;
     }
     document.getElementById("calNumber").textContent=currentNumber;
 }
 
 function clearButton()  {
-    currentNumber=0;
+    currentNumber="0";
     decimalClick=false;
     numberStorage=undefined;
-    stringNumberStorage=undefined;
-    stringCurrent=undefined;
     document.getElementById("calNumber").textContent=0;
     divideForDecimal=10;
     lastNumber=undefined;
 }
 
-function bd_add(number1, number2) {
+function decimal_add(number1, number2) {
     let decimalPointIndex=number1.indexOf(".");
     let decimalPointIndex2=number2.indexOf(".");
     let decimalPointLength=number1.length;
@@ -148,6 +138,48 @@ function bd_add(number1, number2) {
     }
     else    {
         result=firstNumber+secondNumber;
+        stringResult=result.toString();
+    }
+    if(stringResult.charAt(stringResult.length-1)==".") {
+        stringResult=stringResult.replace(".", "");
+    }
+    return stringResult;
+}
+
+function decimal_multi(number1, number2) {
+    let decimalPointIndex=number1.indexOf(".");
+    let decimalPointIndex2=number2.indexOf(".");
+    let decimalPointLength=number1.length;
+    let decimalPointLength2=number2.length;
+    let numbersAfterPoint=decimalPointIndex!=-1?decimalPointLength-1-decimalPointIndex:0;
+    let numbersAfterPoint2=decimalPointIndex2!=-1?decimalPointLength2-1-decimalPointIndex2:0;
+    number1=number1.replace(".", "");
+    number2=number2.replace(".", "");
+    let firstNumber=parseInt(number1);
+    let secondNumber=parseInt(number2);
+    let maxFractionalPart=Math.max(numbersAfterPoint, numbersAfterPoint2);
+    let stringResult;
+    let result;
+    if(maxFractionalPart!=0)    {
+        firstNumber=firstNumber*(10**(maxFractionalPart-numbersAfterPoint));
+        secondNumber=secondNumber*(10**(maxFractionalPart-numbersAfterPoint2));
+        result=firstNumber*secondNumber;
+        stringResult=result.toString();
+        console.log(firstNumber+"=firstnumber");
+        console.log(secondNumber+"=secondneumrb");
+        console.log("result="+stringResult);
+        if(stringResult.length<=maxFractionalPart)   {
+            stringResult = "0." + "0".repeat(maxFractionalPart-stringResult.length) + stringResult;
+        }
+        else {
+            stringResult=stringResult.substring(0, stringResult.length-maxFractionalPart)+"."+stringResult.substring(stringResult.length-maxFractionalPart);
+        }
+        while(stringResult.charAt(stringResult.length-1)=="0")   {
+            stringResult=stringResult.substring(0, stringResult.length-1);
+        }
+    }
+    else    {
+        result=firstNumber*secondNumber;
         stringResult=result.toString();
     }
     if(stringResult.charAt(stringResult.length-1)==".") {
