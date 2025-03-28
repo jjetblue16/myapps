aa616c9b6b4c953a9038c142ec27243912153eb2
jjetblue16-was-here <jaydenzcho@gmail.com>
2025-03-27 20:17:20 -0400

    make grid

--- snake/snake.js
+++ snake/snake.js
@@ -1,7 +1,7 @@
 let width;
 let height;
 
-let snakeBoxSize=13;
+let snakeBoxSize=20;
 
 let background;
 
@@ -10,6 +10,18 @@ let playBoxHeight;
 let playBoxWidth;
 
 let minBorderSize=50;
+
+let movingSteps=5;
+
+let movingInterval=40;
+
+let movingDirection;
+
+let distanceTop;
+let distanceLeft;
+
+let rows;
+let cols;
 
 document.addEventListener('DOMContentLoaded', start);
 
@@ -24,4 +36,39 @@ function start()    {
     playBox.style.height=playBoxHeight+"px";
     playBox.style.width=playBoxWidth+"px";
     console.log(playBoxWidth+","+playBoxHeight)
+    cols=playBoxHeight/snakeBoxSize;
+    rows=playBoxWidth/snakeBoxSize;
+    drawGrid();
+    fillBox(10, 10);
+}
+
+function drawSnake(row, col)    {
+    let snakeBox=document.createElement('div');
+    snakeBox.className="snake";
+    snakeBox.style.height=snakeBoxSize+"px";
+    snakeBox.style.width=snakeBoxSize+"px";
+}
+
+function drawGrid() {
+    for(let a=0; a<rows; a++)   {
+        for(b=0; b<cols; b++)   {
+            let box=document.createElement('div');
+            box.style.height=snakeBoxSize;
+            box.style.width=snakeBoxSize;
+            box.className="box";
+            box.style.top=b*snakeBoxSize+"px";
+            box.style.left=a*snakeBoxSize+"px";
+            box.id="box "+a+","+b;
+            playBox.appendChild(box);
+        }
+    }
+}
+
+function getBlockId(row, col)   {
+    return "box "+row+","+col;
+}
+
+function fillBox(row, col)  {
+    let box=document.getElementById(getBlockId(row, col))
+    box.style.backgroundColor="red";
 }
