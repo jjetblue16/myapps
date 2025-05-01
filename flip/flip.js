a1523da246342f9cd889807470311e6a25d224ae
jjetblue16 <jaydenzcho@gmail.com>
2025-04-30 20:35:40 -0400

    give coin animation

--- flip/flip.js
+++ flip/flip.js
@@ -1,5 +1,32 @@
+let side;
+
+let isHeads=true;
+
+let interval=150;
+
+let button;
+
+document.addEventListener('DOMContentLoaded', start)
+
+function start()    {
+    side=document.getElementById("side");
+    button=document.getElementById("theButton");
+}
+
 function flip() {
-    let side=document.getElementById("side");
+    button.disabled=true;
     let randomSide=Math.floor(Math.random()*2);
-    side.src=randomSide==0 ?"heads.png" :"tails.png";
+    isHeads=randomSide==0;
+    side.src=isHeads ?"heads.png" :"tails.png";
+    let flipTimes=6;
+    for(let i=0; i<flipTimes; i++)  {
+        let enableButton=i==flipTimes-1;
+        setTimeout(animateFlip, i*interval, enableButton);
+    }
+}
+
+function animateFlip(enableButton)  {
+    isHeads=!isHeads;
+    side.src=isHeads ?"heads.png" :"tails.png";
+    button.disabled=!enableButton;
 }
