8c715efd25f42afe19db9859c485b2a7e6f3f2ac
jjetblue16 <jaydenzcho@gmail.com>
2025-05-02 20:08:59 -0400

    add swipe check

--- snake/snake.js
+++ snake/snake.js
@@ -85,6 +85,73 @@ document.addEventListener('keydown', (e)=>{
     }
 })
 
+document.addEventListener('touchstart', handleTouchStart, false);        
+document.addEventListener('touchmove', handleTouchMove, false);
+
+var xDown=null;                                                        
+var yDown=null;
+
+function getTouches(evt) {
+  return evt.touches || evt.originalEvent.touches;
+}                                                     
+                                                                         
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];                                      
+    xDown = firstTouch.clientX;                                      
+    yDown = firstTouch.clientY;                                      
+};                                                
+                                                                         
+function handleTouchMove(evt) {
+    if(!xDown || !yDown) {
+        return;
+    }
+    var xUp=evt.touches[0].clientX;                                    
+    var yUp=evt.touches[0].clientY;
+    var xDiff=xDown-xUp;
+    var yDiff=yDown-yUp;                                                            
+    if(Math.abs(xDiff)>Math.abs(yDiff) ) {
+        if (xDiff>0) {
+            nextDirection=nextDirection!="left" ? "right" : nextDirection;
+            lockDirection=true;
+            if(firstMove) {
+                currentDirection=nextDirection;
+                interval=setInterval(move, movingInterval);
+                firstMove=false;
+            }
+        } else {
+            nextDirection=nextDirection!="right" ? "left" : nextDirection;
+            lockDirection=true;
+            if(firstMove) {
+                currentDirection=nextDirection;
+                interval=setInterval(move, movingInterval);
+                firstMove=false;
+            }
+        }                       
+    }
+    else {
+        if(yDiff>0) {
+            nextDirection=nextDirection!="up" ? "down" : nextDirection;
+            lockDirection=true;
+            if(firstMove) {
+                currentDirection=nextDirection;
+                interval=setInterval(move, movingInterval);
+                firstMove=false;
+            }
+        }
+        else { 
+            nextDirection=nextDirection!="down" ? "up" : nextDirection;
+            lockDirection=true;
+            if(firstMove) {
+                currentDirection=nextDirection;
+                interval=setInterval(move, movingInterval);
+                firstMove=false;
+            }
+        }                                                                 
+    }
+    xDown=null;
+    yDown=null;                                             
+};
+
 function start()    {
     playBox=document.getElementById("playBox");
     background=document.getElementById("background");
