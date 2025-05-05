54e9c57a36ad65f93f949a505fb58a5c9c248d62
jjetblue16 <jaydenzcho@gmail.com>
2025-05-05 19:50:35 -0400

    sound for coinflip

--- flip/flip.js
+++ flip/flip.js
@@ -2,23 +2,28 @@ let side;
 
 let isHeads=true;
 
-let interval=150;
+let interval=270;
 
 let button;
+
+let coinSound;
 
 document.addEventListener('DOMContentLoaded', start)
 
 function start()    {
     side=document.getElementById("side");
     button=document.getElementById("theButton");
+    coinSound=new Audio("coin-sound.mp3");
+    coinSound.load();
 }
 
 function flip() {
     button.disabled=true;
+    coinSound.play();
     let randomSide=Math.floor(Math.random()*2);
     isHeads=randomSide==0;
     side.src=isHeads ?"heads.png" :"tails.png";
-    let flipTimes=6;
+    let flipTimes=13;
     for(let i=0; i<flipTimes; i++)  {
         let enableButton=i==flipTimes-1;
         setTimeout(animateFlip, i*interval, enableButton);
