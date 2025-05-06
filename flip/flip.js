6be81953f285a157798f606d84c5eae5531fa3d5
jjetblue16 <jaydenzcho@gmail.com>
2025-05-05 20:01:47 -0400

    made flip based on the sound

--- flip/flip.js
+++ flip/flip.js
@@ -2,7 +2,7 @@ let side;
 
 let isHeads=true;
 
-let interval=270;
+let interval=250;
 
 let button;
 
@@ -15,23 +15,27 @@ function start()    {
     button=document.getElementById("theButton");
     coinSound=new Audio("coin-sound.mp3");
     coinSound.load();
+    coinSound.addEventListener('playing', (event) => {
+        let randomSide=Math.floor(Math.random()*2);
+        isHeads=randomSide==0;
+        side.src=isHeads ?"heads.png" :"tails.png";
+        let flipTimes=13;
+        for(let i=0; i<flipTimes; i++)  {
+            let enableButton=i==flipTimes-1;
+            setTimeout(animateFlip, i*interval, enableButton);
+        }
+    });
+    coinSound.addEventListener('ended', (event) => {
+        button.disabled=false;
+    });
 }
 
 function flip() {
     button.disabled=true;
     coinSound.play();
-    let randomSide=Math.floor(Math.random()*2);
-    isHeads=randomSide==0;
-    side.src=isHeads ?"heads.png" :"tails.png";
-    let flipTimes=13;
-    for(let i=0; i<flipTimes; i++)  {
-        let enableButton=i==flipTimes-1;
-        setTimeout(animateFlip, i*interval, enableButton);
-    }
 }
 
 function animateFlip(enableButton)  {
     isHeads=!isHeads;
     side.src=isHeads ?"heads.png" :"tails.png";
-    button.disabled=!enableButton;
 }
