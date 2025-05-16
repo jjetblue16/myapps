50a582e79605bbddc8b63a4919a4f103130d0014
jjetblue16 <jaydenzcho@gmail.com>
2025-05-15 20:17:37 -0400

    mineswep

--- minesweeper/minesweeper.js
+++ minesweeper/minesweeper.js
@@ -15,17 +15,25 @@ let gameHeight=450;
 
 let mines=13;
 
-let mode="shovel";
+let mode='shovel';
 
 const bombArray=new Array();
 
 let alive=true;
 
+let lose;
+
+let flagButton;
+let shovelButton;
+
 document.addEventListener('DOMContentLoaded', start);
 
 function start()    {
+    shovelButton=document.getElementById("shovel");
+    flagButton=document.getElementById("flag");
     background=document.getElementById("background");
     gameBox=document.getElementById("gameBox");
+    lose=document.getElementById("lose");
     let sizeInfo=background.getBoundingClientRect();
     width=sizeInfo.width;
     height=sizeInfo.height;
@@ -47,7 +55,10 @@ function start()    {
             console.log(row+","+col);
             if(mode=="shovel")  {
                 open(row, col);
-            }   
+            }
+            else    {
+                flagSpace(row, col);
+            }
         }
     });
 }
@@ -73,20 +84,71 @@ function addBomb()  {
  
 function open(row, col) {
     let block=document.getElementById(getBlockId(row, col));
-    block.style.backgroundColor="rgb(177, 177, 177)";
-    for(let f=0; f<bombArray.length; f++)   {
-        if(row==bombArray[f].theRow && col==bombArray[f].theCol)  {
-            let image=document.createElement('img');
-            image.src="mine3.png";
-            block.appendChild(image);
-            alive=false;
+    if(document.getElementById(getFlagId(row, col))==null)  {
+        block.style.backgroundColor="rgb(177, 177, 177)";
+        for(let f=0; f<bombArray.length; f++)   {
+            if(row==bombArray[f].theRow && col==bombArray[f].theCol)  {
+                let image=document.createElement('img');
+                image.src="mine3.png";
+                block.appendChild(image);
+                alive=false;
+                gameOver();
+            }
+        }
+        if(alive)   {
+            let bomb=getNumberOfBombs(row, col);
+            if(bomb!=0)    {
+                block.textContent=bomb;
+            }
         }
     }
-    if(alive)   {
-        let bomb=getNumberOfBombs(row, col);
-        if(bomb!=0)    {
-            block.textContent=bomb;
+}
+
+function flagSpace(row, col)    {
+    let block=document.getElementById(getBlockId(row, col));
+    let flagPng=document.createElement('img');
+    flagPng.src="flag.png";
+    flagPng.style.width="100%";
+    flagPng.style.height="100%";
+    flagPng.style.top=0;
+    flagPng.style.left=0;
+    flagPng.id=getFlagId(row, col);
+    block.appendChild(flagPng);
+}
+
+function close()    {
+    for(let b=0; b<rows; b++) {
+        for(let c=0; c<cols; c++)   {
+            let block=document.getElementById(getBlockId(b, c));
+            block.style.backgroundColor="gray";
+            block.textContent="";
         }
+    }
+}
+
+function gameOver() {
+    lose.style.display="block";
+}
+
+function restart()  {
+    mode='shovel';
+    bombArray.splice(0, bombArray.length);
+    close();
+    alive=true;
+    lose.style.display="none";
+    flagButton.style.backgroundColor="gray";
+    shovelButton.style.backgroundColor="rgb(177, 177, 177)"
+}
+
+function switchTool(tool)   {
+    mode=tool;
+    if(tool=='shovel')  {
+        shovelButton.style.backgroundColor="rgb(177, 177, 177)";
+        flagButton.style.backgroundColor="gray";
+    }
+    else    {
+        flagButton.style.backgroundColor="rgb(177, 177, 177)";
+        shovelButton.style.backgroundColor="gray";
     }
 }
 
@@ -123,4 +185,8 @@ function getNumberOfBombs(row, col)  {
 
 function getBlockId(row, col)   {
     return "block "+row+","+col;
+}
+
+function getFlagId(row, col)    {
+    return "flag "+row+","+col;
 }
